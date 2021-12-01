b3b4d8b58d81fbe36139890313ee854d4f8137c2
jakub.ciepiela <jakub.ciepiela@gmail.com>
2021-12-01 12:27:37 +0100

    starting GIMP Script-Fu

---
+++ src/main/Script-Fu/extract-faces.scm
@@ -0,0 +1,4 @@
+; GIMP Script-Fu
+; this script will iterate through all the layers of of the image
+; for each layer it will create textual definition tha can be used by 
+; DrawElement procedure from GR0.ACT file
